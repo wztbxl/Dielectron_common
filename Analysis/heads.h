558e1f424bed1aeec830bd0afe6ba649b2598e83
Zhen Wang <wangzhen@rcas6007.rcf.bnl.gov>
2024-10-30 23:17:25 -0400

    dielectron analysis code

---
+++ Analysis/heads.h
@@ -0,0 +1,7 @@
+#ifndef HEADS_h
+#define HEADS_h
+
+TString TotalDayList = "/star/u/wangzhen/run20/Dielectron/DataQA/mTotalDayList.dat"
+TString TotalRunList = "/star/u/wangzhen/run20/Dielectron/DataQA/mTotalRunList.dat"
+
+#endif 
