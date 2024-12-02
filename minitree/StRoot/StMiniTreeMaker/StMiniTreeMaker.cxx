344d56c168dabf9d7b4c041aeb74ef7b5e1aaa79
wztbxl <35267233+wztbxl@users.noreply.github.com>
2024-12-02 18:21:04 +0800

    add more debug information

--- minitree/StRoot/StMiniTreeMaker/StMiniTreeMaker.cxx
+++ minitree/StRoot/StMiniTreeMaker/StMiniTreeMaker.cxx
@@ -491,10 +491,10 @@ Bool_t StMiniTreeMaker::isValidTrack(StPicoTrack *pTrack, TVector3 vtxPos) const
 	if(pt<mMinTrkPt)                            return kFALSE;
 	if(TMath::Abs(eta)>mMaxTrkEta)              return kFALSE;
 	if(pTrack->nHitsFit()<mMinNHitsFit)         return kFALSE;
-	if(pTrack->nHitsFit()*1./pTrack->nHitsMax()<mMinNHitsFitRatio) return kFALSE;
 	if(Debug()){
 	LOG_INFO<<"after reading track"<<endm;
 	}
+	if(pTrack->nHitsFit()*1./pTrack->nHitsMax()<mMinNHitsFitRatio) return kFALSE;
 	if(pTrack->nHitsDedx()<mMinNHitsDedx)       return kFALSE;
 	if(dca>mMaxDca)                             return kFALSE;
 
