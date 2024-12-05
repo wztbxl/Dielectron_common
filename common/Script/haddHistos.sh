db6898d187015275c5c32c4b9f68354429a9195b
wztbxl <35267233+wztbxl@users.noreply.github.com>
2024-12-05 17:28:03 +0800

    hadd histograms

---
+++ common/Script/haddHistos.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+date
+
+function showhelp()
+{
+    cat<<EOF
+=========================================================
+How to Use this macro:
+Example:
+   ./haddHistos.sh "SourceDir" "DestinationDir" NfilesPerJob Options
+   SourceDir: Your input root file dir name(no /);
+   DestinationDir: Your output file dir name(no /);
+   NfilesPerJob: How many files will accumate and submit a job;
+   TEST Mode: runmode = 1;
+   HADD Mode: runmode = 0;
+   Options : please look at hadd options. see:  hadd --help
+=========================================================
+EOF
+}
+
+ if [ $# -ne 4 ]; then
+    showhelp
+    exit 0
+ fi
+
+ origDir=$1
+ echo ${origDir}
+
+ #outDir="output/Cen${1}/"
+ outDir=$2
+
+ echo "SourceDir: ${origDir}"
+ echo "DestinationDir: ${outDir}"
+
+   ~/Scripts/Hadd/hadd.sh 0 ${origDir} ${outDir} 50
+
